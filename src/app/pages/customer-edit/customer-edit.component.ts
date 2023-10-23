7bdfc7b4659525e7972d9b67f807bbbd4f2c7975
Lucas Ambrosio <lu.amb99@gmail.com>
2023-10-23 17:53:17 -0300

    first commit

---
+++ src/app/pages/customer-edit/customer-edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Customer } from 'src/app/model/customer';
+
+@Component({
+  selector: 'app-customer-edit',
+  templateUrl: './customer-edit.component.html',
+  styleUrls: ['./customer-edit.component.css'],
+})
+export class CustomerEditComponent implements OnInit {
+  id: number = -2;
+  isNewCustomer: boolean = true;
+  customer: Customer = {
+    id: -1,
+    name: '',
+    dateOfBirth: new Date(),
+    email: '',
+  };
+
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    const getId = this.route.snapshot.paramMap.get('id');
+    if (getId) this.id = parseInt(getId);
+  }
+
+  salvar() {
+    debugger;
+  }
+}
